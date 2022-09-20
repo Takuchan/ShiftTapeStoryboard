54ad460de28852e530bb845a79905309d5de2300
Takuchan <awatykhall850@outlook.com>
2022-09-20 19:56:23 +0900

    Initial Commit

---
+++ ShiftTapeStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShiftTapeStoryboard
+//
+//  Created by 松村拓洋 on 2022/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
