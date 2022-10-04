8d8b5122062822cc2ac980ba9d46f2cd51cb2a92
Takuchan <awatykhall850@outlook.com>
2022-10-04 23:57:47 +0900

    レイアウトを変えた

--- ShiftTapeStoryboard/ViewController.swift
+++ ShiftTapeStoryboard/ViewController.swift
@@ -12,8 +12,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setLeftTitle("あほ")
     }
 
 
+
+    
+   
 }
 
+
+extension UIViewController {
+    /// NavigationBarの左にローカライズされたタイトルを表示する
+    func setLeftTitle(_ localizedStringKey: String) {
+        let titleLabel = UILabel(frame: CGRect(x: 4, y: 0, width: view.frame.width, height: 28))
+        titleLabel.text = localizedStringKey.lowercased()
+        titleLabel.textAlignment = .left
+        titleLabel.font = .systemFont(ofSize: 17, weight: .semibold)
+        titleLabel.textColor = .white
+        navigationItem.titleView = titleLabel
+    }
+}
